31cb4c5eac137b2958864d9e3235f65769704aeb
Trine Motion Developers <trinemotion@gmail.com>
2026-03-15 18:21:33 -0300

    major change

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,27 @@ function separateContent(){
     for(let word = 0; word <= mainArray.length-1; word++){
     var span = document.createElement('span');
     span.textContent = mainArray[word];
+    span.onclick = () => loadPopUp(mainArray[word]);
     contentDiv.appendChild(span); 
     };
+};
+
+function loadPopUp(word){
+    openPopUp(word);
+    findWord(word);
+};
+
+function openPopUp(word){
+    var modal = document.getElementsByClassName("modal")[0];
+    var title = document.getElementById("content-word");
+    title.innerText = word;
+    modal.style.display = "block";
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    } 
+};
+
+function findWord(word){
 };
